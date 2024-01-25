43d9a6b690c5ae84de2841ec5d3c717f66c2e03f
einnad <146109295+einnad@users.noreply.github.com>
2024-01-25 17:35:35 +0000

    Fix database to link with env

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
+import env from "dotenv";
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
 
+env.config();
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// const db = new pg.Client({
-//   name: "postgres",
-//   host: "localhost",
-//   database: "",
-//   password: "",
-//   port: 5432,
-// });
+const db = new pg.Client({
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
+});
 
-// db.connect();
+db.connect();
 
 app.listen(port, () => {
   console.log(`Server running on ${port}`);
