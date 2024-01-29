5c78980ba6d9400db686896a3d6560d8c580c473
einnad <146109295+einnad@users.noreply.github.com>
2024-01-29 12:32:55 +0000

    Add create review feature, updates reviews db

--- index.js
+++ index.js
@@ -20,7 +20,15 @@ const db = new pg.Client({
 
 db.connect();
 
+// join db reviews/readers when implementing more readers
+
 // function to find reader from reviews
+async function getReader(reader) {
+  const result = await db.query("SELECT id FROM readers WHERE name = $1", [
+    reader,
+  ]);
+  return result.rows[0].id;
+}
 
 app.listen(port, () => {
   console.log(`Server running on ${port}`);
@@ -35,3 +43,21 @@ app.get("/", async (req, res) => {
     console.log(err);
   }
 });
+
+app.post("/review", async (req, res) => {
+  try {
+    const readerId = await getReader(req.body.reader);
+    // console.log(readerId);
+    await db.query(
+      "INSERT INTO reviews (reader_id, title, author, review) VALUES ($1, $2, $3, $4)",
+      [readerId, req.body.title, req.body.author, req.body.review]
+    );
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// test data
+// A Big Ship at the Edge of the Universe
+// The characters are captivating; their stories soon become the heart of the adventure.
